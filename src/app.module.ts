830f34125d6051e3054caf9b56f9ebf4ae6329c9
iKamranShahzad <mailkamranshahzad@gmail.com>
2025-04-18 22:38:43 +0500

    Add AuthCheckMiddleware for request logging and authorization header validation; update AppModule to apply middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SumServiceService } from './sum-service/sum-service.service';
 import { SaynameModule } from './sayname/sayname.module';
+import { AuthCheckMiddleware } from './middleware/authcheck.middleware';
 
 @Module({
   imports: [SaynameModule],
   controllers: [AppController],
   providers: [AppService, SumServiceService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthCheckMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
---
+++ src/middleware/authcheck.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class AuthCheckMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(AuthCheckMiddleware.name);
+
+  use(req: Request, res: Response, next: NextFunction) {
+    if (req.originalUrl !== '/favicon.ico') {
+      this.logger.log(
+        `\n[${req.method}] ${req.originalUrl}\nHeaders:\n${JSON.stringify(req.headers, null, 2)}\n`,
+      );
+
+      if (!req.headers.authorization) {
+        this.logger.warn('Missing Authorization header');
+      }
+    }
+    next();
+  }
+}
