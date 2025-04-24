dd702637b8fa84e40b486e2d28c05cb5206d2c94
iKamranShahzad <mailkamranshahzad@gmail.com>
2025-04-24 23:23:50 +0500

    Refactor ArangoDB provider to configure authentication directly in the Database instance

--- src/database/arangodb.provider.ts
+++ src/database/arangodb.provider.ts
@@ -4,11 +4,12 @@ dotenv.config();
 
 const db = new Database({
   url: process.env.ARANGODB_URL || 'http://localhost:8529',
+  databaseName: process.env.ARANGODB_DB || 'test',
+  auth: {
+    username: process.env.ARANGODB_USER || 'root',
+    password: process.env.ARANGODB_PASS || '',
+  },
 });
-db.useBasicAuth(
-  process.env.ARANGODB_USER || 'root',
-  process.env.ARANGODB_PASS || '',
-);
 
 const dbName = process.env.ARANGODB_DB || 'test';
 export const database = db.database(dbName);
